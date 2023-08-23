c6d2363ecdd19126b42d4634160aa972431d310a
MonikaWebethics <monika.webethics@gmail.com>
2023-08-23 12:49:32 +0530

    New Commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
